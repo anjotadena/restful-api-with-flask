dd4f40ce73debc11e07a3dd93d25ddd5ea2b4717
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-11 07:54:40 +0800

    Setting up stores endpoints

--- app.py
+++ app.py
@@ -8,4 +8,23 @@ print(f"__name__ = {__name__}")
 def home():
     return "Hello, World!"
 
+# GET - used to receive data
+# POST - used to store data
+
+@app.route('/stores', methods=['POST'])
+def create_store():
+    pass
+
+@app.route('/stores/<string:name>')
+def get_store(name):
+    pass
+
+@app.route('/stores')
+def get_stores():
+    pass
+
+@app.route('/stores/<string:name>/items', methods=['POST'])
+def create_stores_item():
+    pass
+
 app.run(port=5000)
