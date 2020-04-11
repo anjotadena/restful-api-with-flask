a849203e1a87b52eeaeb60da1e74814f6e48e945
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-11 14:37:18 +0800

    Setup environment config

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+app.config.from_object('project.config.DevelopmentConfig')
+
 @app.route('/users/ping', methods=['GET'])
 def user_ping():
     return jsonify({
